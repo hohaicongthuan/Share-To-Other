35a328174b15c75293ca3cf4a58bf692c51af9de
hohaicongthuan <hohaicongthuan@gmail.com>
2020-09-29 12:26:01 +0700

    Add files

---
+++ Thiet ke he thong so/Library/CounterwParallelLoad/CounterwParallelLoad.v
@@ -0,0 +1,46 @@
+module CounterwParallelLoad(data_o, data_i, d_i, e_i, Clk, load_i, cout);
+    parameter BUS_WIDTH = 8; // Define bus width here
+    
+    input d_i, e_i, load_i, Clk;
+    input [7:0] data_i;
+    output [7:0] data_o;
+    output cout;
+
+    wire d_i, load_i, Clk, cout;
+    wire [7:0] data_i;
+    wire [7:0] data_o;
+
+    // Internal wires declaration
+    wire    [7:0] HAS_Output;
+    wire    [7:0] Selector_Output;
+    wire    [7:0] HAS_Cout;
+	 wire		[7:0] Q_N;
+    wire    d_i_neg;
+
+    not(d_i_neg, d_i);
+
+    HAS HAS_Inst [7:0](
+			.Q(data_o[7:0]),
+			.Cin({HAS_Cout[6:0], e_i}),
+			.Cout({cout, HAS_Cout[6:0]}),
+			.O(HAS_Output[7:0]),
+			.D(d_i),
+			.D_N(d_i_neg),
+			.Q_N(Q_N)
+	);
+
+    Selector Selector_Inst [7:0](
+        .data_o(Selector_Output[7:0]),
+        .dataA_i(HAS_Output[7:0]),
+        .dataB_i(data_i[7:0]),
+        .select_i(load_i)
+    );
+
+    DFlipFlop DFF_Inst [7:0](
+        .Q(data_o[7:0]),
+        .Q_N(Q_N[7:0]),
+        .D(Selector_Output[7:0]),
+        .clk(Clk)
+    );
+
+endmodule
---
+++ Thiet ke he thong so/Library/CounterwParallelLoad/Submodules/DFlipFlop.v
@@ -0,0 +1,19 @@
+// D flip-flop
+// Q_N = complement of Q
+
+module DFlipFlop(Q, Q_N, D, clk);
+    input D, clk;
+    output Q, Q_N;
+
+    wire Q, Q_N, clk, D;
+
+    // Internal wires declaration
+    wire w1, w2, w3, w4, D_N;    // D_N = complement of D
+
+    not(D_N, D);
+    nand(w1, D, clk);
+    nand(w2, D_N, clk);
+    nand(Q, Q_N, w1);
+    nand(Q_N, Q, w2);
+
+endmodule
