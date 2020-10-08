2ac58897ee373326265bfe4c364f02724b4ac686
hohaicongthuan <hohaicongthuan@gmail.com>
2020-10-08 09:41:42 +0700

    Minor bug fix/Add more files

--- Verilog Assignments/RAM/Submodules/word.v
+++ Verilog Assignments/RAM/Submodules/word.v
@@ -2,12 +2,12 @@
 // The number of bits is defined in parameter below
 
 module word(RowSel_i, data_i, data_o, WEn_i);
+	// Define how big is a word here
+	parameter BUSWIDTH = 8;
+	
 	input RowSel_i, WEn_i;
 	input [BUSWIDTH - 1:0] data_i;
 	output [BUSWIDTH - 1:0] data_o;
-	
-	// Define how big is a word here
-	parameter BUSWIDTH = 8;
 	
 	MemoryCell MC_Inst[BUSWIDTH - 1:0](
 		.RowSel_i(RowSel_i),
