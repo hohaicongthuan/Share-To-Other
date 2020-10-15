279ff6d381617488b609777ab5eda99e7a14ed65
hohaicongthuan <hohaicongthuan@gmail.com>
2020-10-15 11:51:07 +0700

    Add RegisterFile

--- Verilog Assignments/RegisterFiles/Submodules/RegisterFile.v
+++
@@ -1,8 +0,0 @@
-module RegisterFile(data_i, data_o, WriteAddr, WriteEn, ReadA, ReadAEn, ReadB, ReadBEn, Clk);
-	input WriteEn, ReadAEn, ReadBEn, Clk;
-	input [31:0] data_i;
-	input [4:0] WriteAddr, ReadA, ReadB;
-	output [31:0] data_o;
-	
-	
-endmodule
