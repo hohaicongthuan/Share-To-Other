ea93a580c740026ace40e26e27ebc01f290c0cfa
rock-on <36922874+AcezukyRO@users.noreply.github.com>
2020-10-28 16:02:26 +0700

    Add .vo and .sdo file. Update testbench.v for RAM

--- Verilog Assignments/RAM/Testbench.v
+++ Verilog Assignments/RAM/Testbench.v
@@ -6,19 +6,28 @@ module Testbench();
     reg [9:0] addr_i;
     wire [7:0] data_o;
     
-    parameter waittime = 50;
-    
+    parameter waittime = 20;
+	 integer i;
+
+    /*initial begin
+      Clk = 1'b0; // Initial value of the clock signal
+      forever #10 Clk = ~Clk; // Generates clock pulses forever
+    end*/
+
     initial begin
         data_i = 0;
         addr_i = 0;
         WEn_i = 0;
         #waittime;
-        data_i = 8'd9;
-        addr_i = 10'd100;
-        WEn_i = 1'b1;
-        #waittime;
-        WEn_i = 1'b0;
-        #waittime;
+
+        for (i = 0; i < 1023; i = i + 1) begin
+            addr_i = i;
+            data_i = i;
+            WEn_i = 1'b1;
+            #waittime;
+            WEn_i = 1'b0;
+            #waittime;
+        end
         #2000 $finish;
     end
 
