48ad538253e1f72256a57c84d014af40390c9ccd
hohaicongthuan <hohaicongthuan@gmail.com>
2020-12-29 20:55:27 +0700

    Create AlgorithmVerification.py

---
+++ Group Project/AlgorithmVerification.py
@@ -0,0 +1,30 @@
+# This script verify Newton's algorithm in approxiating
+# the square root of a number
+
+import math
+
+def SquareRoot(n):
+    x = n
+    root = 0.0
+    for i in range(12):
+        root = 0.5 * (x + (n / x))
+        x = root
+
+    return root
+
+# Main script
+num = 0.0
+NumOfCase = 0
+AverageError = 0
+for i in range(1000000000):
+    num += 0.0001
+    print("Test Number:\t\t\t", num)
+    print("Square Root:\t\t\t", math.sqrt(num))
+    print("Result from the Algorithm:\t", SquareRoot(num))
+    print("Error:\t\t\t\t", abs(math.sqrt(num) - SquareRoot(num)))
+    print("")
+
+    AverageError += abs(math.sqrt(num) - SquareRoot(num))
+    NumOfCase += 1
+
+print("Average Error:\t\t", AverageError / NumOfCase)
